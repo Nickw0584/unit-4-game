fcf745927fce73a3116ca475c84dbe733880bf3d
Nick Wilson <nickw0584@icloud.com>
2019-08-08 00:12:22 -0500

    new dates

--- assests/javascript/game.js
+++ assests/javascript/game.js
@@ -38,53 +38,57 @@ return Math.floor(Math.random() * (max - min) + min);
 
 console.log(getRndNumber(19,120))
 
+//document.addEventListener("click",function(){
+   // document.getElementById("crystal one");});
+    //buttons.innerHTML = '<img src="https://i.ya-webdesign.com/images/cartoon-diamond-png-4.png"/>';})
+    
+    console.log(getRndNumber(1,19))
+    
+   // document.addEventListener("click",function(){
+   // document.getElementById("crystal two");});
+    //buttons.innerHTML = '<img src="https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-polygonal-red-cartoon-gemstone-image_1405387.jpg"/>';})
+    
+    console.log(getRndNumber(1,19))
+    
+   // document.addEventListener("click",function(){
+   // document.getElementById("crystal three");});
+    //buttons.innerHTML = '<img src="https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-green-crystal-gems-illustration-image_1405421.jpg"/>';})
+    console.log(getRndNumber(1,19))
+    
+    //document.addEventListener("click",function(){
+    //document.getElementById("crystal four");});
+    //buttons.innerHTML = '<img src="https://longfordpc.com/images/diamonds-clipart-amethyst-9.png"/>';});
+    console.log(getRndNumber(1,19))
+    
 const imageCrystals = document.createElement("img");
 imageCrystals.classList.add("crystal-image");
 
-imageCrystals.setAttribute("src","https://i.ya-webdesign.com/images/cartoon-diamond-png-4.png",
-"https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-polygonal-red-cartoon-gemstone-image_1405387.jpg",
-"https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-green-crystal-gems-illustration-image_1405421.jpg",
-"https://longfordpc.com/images/diamonds-clipart-amethyst-9.png");
-//imageCrystals.setAttribute("src","https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-polygonal-red-cartoon-gemstone-image_1405387.jpg");
-//imageCrystals.setAttribute("src","https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-green-crystal-gems-illustration-image_1405421.jpg");
-//imageCrystals.setAttribute("src","https://longfordpc.com/images/diamonds-clipart-amethyst-9.png");
+imageCrystals.setAttribute("src","https://i.ya-webdesign.com/images/cartoon-diamond-png-4.png");
 
-imageCrystals.setAttribute("crystalnumber", getRndNumber(19,120));
+imageCrystals.setAttribute("src","https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-polygonal-red-cartoon-gemstone-image_1405387.jpg");
+
+imageCrystals.setAttribute("src","https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-green-crystal-gems-illustration-image_1405421.jpg");
+
+imageCrystals.setAttribute("src","https://longfordpc.com/images/diamonds-clipart-amethyst-9.png");
+
+imageCrystals.setAttribute("crystalnumber", getRndNumber[19,120]);
 
 document.getElementById("crystals").append(imageCrystals);
 
-document.querySelectorAll(".crystals-image").forEach(function(){
+document.querySelectorAll(".crystals-image").forEach(function(){})
     
-document.addEventListener("click", function(){
+//document.addEventListener("click", function(){
 
-    }
-
-}
-
-document.addEventListener("click",function(){
-document.getElementById("crystal one");});
-//buttons.innerHTML = '<img src="https://i.ya-webdesign.com/images/cartoon-diamond-png-4.png"/>';})
-
-console.log(getRndNumber(1,19))
-
-document.addEventListener("click",function(){
-document.getElementById("crystal two");});
-//buttons.innerHTML = '<img src="https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-polygonal-red-cartoon-gemstone-image_1405387.jpg"/>';})
-
-console.log(getRndNumber(1,19))
-
-document.addEventListener("click",function(){
-document.getElementById("crystal three");});
-//buttons.innerHTML = '<img src="https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-green-crystal-gems-illustration-image_1405421.jpg"/>';})
-console.log(getRndNumber(1,19))
-
-document.addEventListener("click",function(){
-document.getElementById("crystal four");});
-//buttons.innerHTML = '<img src="https://longfordpc.com/images/diamonds-clipart-amethyst-9.png"/>';});
-console.log(getRndNumber(1,19))
+  //  }
 
 
 
+ // if (counter === main){
+      //  alert("You Won!);
+           
+// }
+// else if(counter !== main){
+    // alert("Sorry, you lose!!!!");
 
 
 
@@ -92,3 +96,4 @@ console.log(getRndNumber(1,19))
 //https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-polygonal-red-cartoon-gemstone-image_1405387.jpg
 //https://png.pngtree.com/element_our/20190602/ourlarge/pngtree-green-crystal-gems-illustration-image_1405421.jpg
 //https://longfordpc.com/images/diamonds-clipart-amethyst-9.png
+    
